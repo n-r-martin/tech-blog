5cf0675690da75c501ddc8b86d2f8df7f67fcbc9
Nick Martin <martin.nicholas.r@gmail.com>
2022-04-03 22:21:00 -0600

    landing page route rendering landing page view

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,23 +3,24 @@ const { ModelOne, ModelTwo } = require('../models');
 
 router.get('/', async (req, res) => {
   try {
-    const queryOneData = await ModelOne.findAll();
-    const ContentData = {
-      "id": queryOneData[0].dataValues.id,
-      "content": queryOneData[0].dataValues.content,
-    };
+    // const queryOneData = await ModelOne.findAll();
+    // const ContentData = {
+    //   "id": queryOneData[0].dataValues.id,
+    //   "content": queryOneData[0].dataValues.content,
+    // };
 
-    const queryTwoData = await ModelTwo.findAll();
-    const subContentData = {
-      "id": queryTwoData[0].dataValues.id,
-      "SubContent": queryTwoData[0].dataValues.SubContent,
-      "one_id": queryTwoData[0].dataValues.one_id,
-    };
+    // const queryTwoData = await ModelTwo.findAll();
+    // const subContentData = {
+    //   "id": queryTwoData[0].dataValues.id,
+    //   "SubContent": queryTwoData[0].dataValues.SubContent,
+    //   "one_id": queryTwoData[0].dataValues.one_id,
+    // };
 
-    res.render('content', { 
-      ContentData, 
-      subContentData 
-    });
+    // res.render('content', { 
+    //   ContentData, 
+    //   subContentData 
+    // });
+    res.render('landingpage');
   } catch (err) {
     res.status(500).json(err);
   }
