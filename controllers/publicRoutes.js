bd7224aadd5967920431201e8857957520dfeac1
Nick Martin <martin.nicholas.r@gmail.com>
2022-04-03 22:33:28 -0600

    login and signup routes are working, updated navigation

---
+++ controllers/publicRoutes.js
@@ -0,0 +1,73 @@
+const router = require('express').Router();
+const { ModelOne, ModelTwo } = require('../models');
+
+router.get('/', async (req, res) => {
+  try {
+    // const queryOneData = await ModelOne.findAll();
+    // const ContentData = {
+    //   "id": queryOneData[0].dataValues.id,
+    //   "content": queryOneData[0].dataValues.content,
+    // };
+
+    // const queryTwoData = await ModelTwo.findAll();
+    // const subContentData = {
+    //   "id": queryTwoData[0].dataValues.id,
+    //   "SubContent": queryTwoData[0].dataValues.SubContent,
+    //   "one_id": queryTwoData[0].dataValues.one_id,
+    // };
+
+    // res.render('content', { 
+    //   ContentData, 
+    //   subContentData 
+    // });
+    res.status(200).render('landingpage');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', async (req, res) => {
+  try {
+    res.status(200).render('login');
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+router.get('/signup', async (req, res) => {
+  try {
+    res.status(200).render('signup');
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+// router.get('/loggedin', async (req, res) => {
+//   try {
+//     const queryOneData = await ModelOne.findAll();
+//     const ContentData = {
+//       "id": queryOneData[0].dataValues.id,
+//       "content": queryOneData[0].dataValues.content,
+//     };
+
+//     const queryTwoData = await ModelTwo.findAll();
+//     const subContentData = {
+//       "id": queryTwoData[0].dataValues.id,
+//       "SubContent": queryTwoData[0].dataValues.SubContent,
+//       "one_id": queryTwoData[0].dataValues.one_id,
+//     };
+
+//     res.render('logged-in-content', { 
+//       ContentData, 
+//       subContentData,
+//       logged_in: req.session.logged_in 
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+
+
+module.exports = router;
+
